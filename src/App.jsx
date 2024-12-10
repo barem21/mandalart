fe0f0482ce9fd4ed47af7e74614d975e5af8d082
barem21 <barem210@gmail.com>
2024-12-10 18:25:54 +0900

    [TODO 프로젝트] 회원가입폼 추가

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,13 @@ import "./App.css";
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
 import LoginPage from "./pages/LoginPage";
+import JoinPage from "./pages/JoinPage";
 
 function App() {
   return (
     <>
       <Header />
-      <LoginPage />
+      <JoinPage />
       <Footer />
     </>
   );
