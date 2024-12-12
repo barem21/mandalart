7991ae86f1cf80bc5769d48ee1edfa5c1c014ddd
barem21 <barem210@gmail.com>
2024-12-12 11:30:16 +0900

    로그인, 회원가입 페이지 경로 변경

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import "./App.css";
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
-import LoginPage from "./pages/LoginPage";
+import LoginPage from "./pages/member/LoginPage";
 
 function App() {
   return (
