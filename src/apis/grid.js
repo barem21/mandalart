57e8c66421e7c8d7157899ca6b2f66fbdbee259b
woogi95 <sdw951216@gmail.com>
2024-12-27 16:28:22 +0900

    만다라트 axios 작업중

--- src/apis/grid.js
+++ src/apis/grid.js
@@ -1,10 +1,19 @@
 import axios from "axios";
 
-export const getGridData = async () => {
+export const getGridData = async data => {
   try {
-    const res = await axios.get(`api/mand`);
+    const res = await axios.get(`api/mand?&projectId={datas.projectId}`);
     return res;
   } catch (error) {
     console.log("404 데이터 불러오기 실패", error);
+  }
+};
+
+export const postGridData = async data => {
+  try {
+    const res = await axios.post(`api/mand?projectId={datas.projectId}`);
+    return res;
+  } catch (error) {
+    console.log("네트워크 연결을 확인 해주세요.", error);
   }
 };
