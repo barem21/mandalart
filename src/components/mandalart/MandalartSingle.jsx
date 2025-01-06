aa4fca10b9ac1da0a15b95bbe540eb4491886567
barem21 <barem210@gmail.com>
2025-01-06 23:11:42 +0900

    [manda] 만다라트 뷰 css 수정

--- src/components/mandalart/MandalartSingle.jsx
+++ src/components/mandalart/MandalartSingle.jsx
@@ -1,77 +1,78 @@
-
-import styles from './MandalartSingle.module.css'
+import styles from "./MandalartSingle.module.css";
 import { useLocation } from "react-router-dom";
 
-function MandalartSingle({jsonData, openModal}) {
-    // 변수선언
+function MandalartSingle({ jsonData, openModal }) {
+  // 변수선언
 
-    const location = useLocation(); // 현재 경로 정보
-    const modalOpenPath = "/myplan/edit"; // 모달이 열려야 하는 경로
+  const location = useLocation(); // 현재 경로 정보
+  const modalOpenPath = "/myplan/edit"; // 모달이 열려야 하는 경로
 
-    let mandalatSingleArr =  Array.from(Array(3).fill({}), () => new Array(3).fill({})); // 3 x 3 빈배열 생성
+  let mandalatSingleArr = Array.from(Array(3).fill({}), () =>
+    new Array(3).fill({}),
+  ); // 3 x 3 빈배열 생성
 
+  // 함수 정의
 
+  // 버튼 클릭시 모달 표시
+  const handleClick = value => {
+    openModal(value);
+  };
 
-    // 함수 정의
+  // 행(y)과 열(x)을 바탕으로 테이블 셀을 생성합니다.
+  const renderTable = () => {
+    return mandalatSingleArr.map((valueArr, rowIndex) => (
+      <tr key={`row-${rowIndex}`}>
+        {valueArr.map((value, colIndex) => {
+          return (
+            <td
+              style={{ backgroundColor: value.bgColor }}
+              key={`cell-${rowIndex}-${colIndex}`}
+            >
+              <button
+                style={{
+                  cursor:
+                    location.pathname === modalOpenPath ? "pointer" : "default",
+                }}
+                onClick={() => handleClick(value)}
+              >
+                {value.title}
+              </button>
+            </td>
+          );
+        })}
+      </tr>
+    ));
+  };
 
-    // 버튼 클릭시 모달 표시
-    const handleClick = (value) => {
-        openModal(value);
-    };
+  // 컴포넌트 로직 처리
 
-    // 행(y)과 열(x)을 바탕으로 테이블 셀을 생성합니다.
-    const renderTable = () => {
-        return mandalatSingleArr.map((valueArr, rowIndex) => (
-        <tr key={`row-${rowIndex}`}>
-            {valueArr.map((value, colIndex) => {
-                return (
-                <td 
-                    style={{"backgroundColor" : value.bgColor}}
-                    key={`cell-${rowIndex}-${colIndex}`}
-                >
-                    <button style={{ cursor : location.pathname === modalOpenPath ? 'pointer' : 'default' }} onClick={() => handleClick(value)}
-                       
-                    >{value.title}</button>
-                </td>
-                )
-            })}
-        </tr>
-        ));
-    };
+  if (jsonData.mandalartId != null) {
+    mandalatSingleArr[1][1] = jsonData;
 
+    let idxX = 0;
+    let idxY = 0;
 
-    
-    // 컴포넌트 로직 처리
+    for (const item of jsonData.child) {
+      if (idxX == 1 && idxY == 1) {
+        idxX++;
+      }
 
-    if(jsonData.mandalartId != null) {
-        mandalatSingleArr[1][1] = jsonData;
+      mandalatSingleArr[idxY][idxX] = item;
 
-        let idxX = 0;
-        let idxY = 0;
+      idxX++;
 
-        for(const item of jsonData.child) {
-            if(idxX == 1 && idxY == 1) {
-                idxX++;
-            }
-            
-            mandalatSingleArr[idxY][idxX] = item;
-            
-            idxX++;
-
-            if(idxX == 3) {
-                idxX = 0;
-                idxY++;
-            }
-        }
+      if (idxX == 3) {
+        idxX = 0;
+        idxY++;
+      }
     }
-        
-    return (
-        <table className={styles.MandalartSingleTable}>
-        <tbody>
-            {renderTable()}
-        </tbody>
-        </table>
-    );
+  }
+
+  return (
+    <table className={styles.MandalartSingleTable}>
+      <tbody>{renderTable()}</tbody>
+    </table>
+  );
 }
 
 export default MandalartSingle;
